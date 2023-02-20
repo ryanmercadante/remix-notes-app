0794b967ffc7a7474c84e8146e92391a22356c2b
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2023-02-20 12:48:39 -0500

    fetch data for dynamic route

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,20 +1,35 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import type { Note } from "~/data/notes";
+import { getStoredNotes } from "~/data/notes";
 
 import styles from "~/styles/note-details.css";
 
+type LoaderData = {
+  note: Note;
+};
+
 export default function NoteDetailsPage() {
+  const data = useLoaderData() as LoaderData;
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back to all Notes</Link>
         </nav>
-        <h1>Note Title</h1>
+        <h1>{data.note.title}</h1>
       </header>
-      <p id="note-details-content">Note Content</p>
+      <p id="note-details-content">{data.note.content}</p>
     </main>
   );
 }
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const notes = await getStoredNotes();
+  const selectedNote = notes.find((n) => n.id === params.noteId);
+  return json<LoaderData>({ note: selectedNote! });
+};
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
